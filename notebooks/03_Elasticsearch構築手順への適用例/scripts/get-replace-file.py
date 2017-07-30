3e8c4534bcda28cf5d7d42b325e945846a5ac372
yacchin1205 <yazawa@yzwlab.net>
2017-07-30 18:32:41 +0900

    Update notebooks to use standalone Notebook Server

--- notebooks/03_Elasticsearch構築手順への適用例/scripts/get-replace-file.py
+++ notebooks/03_Elasticsearch構築手順への適用例/scripts/get-replace-file.py
@@ -5,7 +5,13 @@ import os
 def replace_file(src_path, dst_path, host_ip_list):
     with open(src_path, 'r') as src_file:
         with open(dst_path, 'w') as dst_file:
+            for host in sorted(host_ip_list.keys()):
+                (name, ip_addr, private_ip) = host_ip_list[host]
+                dst_file.write('''[{name}]
+{ip_addr}
+
+'''.format(name=name, ip_addr=ip_addr))
             for line in src_file:
-                for host, ip_addr in host_ip_list.items():
-                    line = re.sub('{' + host + '}', ip_addr, line)
+                for host, (name, ip_addr, private_ip) in host_ip_list.items():
+                    line = re.sub('{' + host + '}', name, line)
                     dst_file.write(line)
