864f10669895ac28f17167a2be84bcab7fd9e389
yacchin1205 <yazawa@yzwlab.net>
2018-05-08 17:34:24 +0900

    Disable delete_to_trash to prevent an error while deleting

--- conf/jupyter_notebook_config.py
+++ conf/jupyter_notebook_config.py
@@ -2,9 +2,9 @@ import os
 c.NotebookApp.ip = '*'
 c.MultiKernelManager.kernel_manager_class = 'lc_wrapper.LCWrapperKernelManager'
 c.KernelManager.shutdown_wait_time = 10.0
+c.FileContentsManager.delete_to_trash = False
 
 if 'PASSWORD' in os.environ:
     from notebook.auth import passwd
     c.NotebookApp.password = passwd(os.environ['PASSWORD'])
     del os.environ['PASSWORD']
-
