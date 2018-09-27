12f7fa230c80207077f74e0223025abea2aa9f59
shingo78 <shingo.nakagawa@gmail.com>
2018-09-27 18:39:41 +0900

    Mitigate cmd variable issue

--- conf/10-custom-get_ipython_system.py
+++ conf/10-custom-get_ipython_system.py
@@ -9,7 +9,7 @@ get_ipython().system = lambda x: get_ipython_system(x)
 # interactiveshell.py's system_piped() function comment saids:
 # "we store the exit_code in user_ns."
 # this function check the exit_code and raise exception if it is not 0.
-def get_ipython_system(cmd):
-    save_get_ipython_system(cmd)
+def get_ipython_system(_cmd):
+    save_get_ipython_system(_cmd)
     if get_ipython().user_ns['_exit_code'] != 0:
         raise RuntimeError('Unexpected exit code: %d' % get_ipython().user_ns['_exit_code'])
