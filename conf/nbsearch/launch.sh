00c354c0145c7a9e61e77b7c5ca3cb3cf07905b3
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2020-04-17 17:11:34 +0900

    Fix launch script

--- conf/nbsearch/launch.sh
+++ conf/nbsearch/launch.sh
@@ -6,10 +6,8 @@ set -xe
 if [[ "${NBSEARCHDB_HOSTNAME}" == "127.0.0.1" ]] ; then
     sed "s,{NB_USER},$NB_USER," /opt/nbsearch/mongod.conf.template > /opt/nbsearch/mongod.conf
     ln -s /opt/nbsearch/mongodb.conf /opt/nbsearch/conf.d/mongodb.conf
-    if [ $(id -u) == 0 ] ; then
-        mkdir -p /home/$NB_USER/.nbsearch/mongodb/
-        chown $NB_UID:$NB_GID -R /home/$NB_USER/.nbsearch
-    fi
+    mkdir -p /home/$NB_USER/.nbsearch/mongodb/
+    chown $NB_UID:$NB_GID -R /home/$NB_USER/.nbsearch
 fi
 
 # Launch Supervisord (FS monitor and MongoDB(if needed))
