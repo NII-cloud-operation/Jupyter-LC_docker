84247c9f15ea2a597a0f98810ffc5d1b7641b0d2
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2020-04-27 18:09:35 +0900

    Fix lsyncd for custom NB_USER

--- conf/nbsearch/launch.sh
+++ conf/nbsearch/launch.sh
@@ -4,5 +4,9 @@ set -xe
 
 # Launch auto updater
 if [[ ! -z "${NBSEARCHDB_AUTO_UPDATE}" ]] ; then
-  lsyncd /opt/nbsearch/update-index.lua
+  if [ $(id -u) == 0 ] ; then
+    sudo -E -u $NB_USER lsyncd /opt/nbsearch/update-index.lua
+  else
+    lsyncd /opt/nbsearch/update-index.lua
+  fi
 fi
