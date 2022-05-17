ddac2bd52bc31a9eacfae7c8a22a5ed93993f864
yacchin1205 <yazawa@yzwlab.net>
2022-05-17 09:49:59 +0900

    Upgrade base image

---
+++ conf/99-run-supervisor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -xe
+
+# For Solr
+supervisord -c /home/jovyan/.nbsearch/supervisor.conf
+
+if [[ ! -f /home/$NB_USER/.nbsearch/config_local.py ]] ; then
+    while ! nc -z localhost 8983; do
+      sleep 0.5
+    done
+    while ! nc -z localhost 9000; do
+      sleep 0.5
+    done
+    while ! curl http://localhost:8983/solr/jupyter-cell/admin/ping | grep '"status":"OK"'; do
+      sleep 0.5
+    done
+    jupyter nbsearch update-index --debug /home/$NB_USER/.jupyter/jupyter_notebook_config.py local &
+fi
+
+export SUPERVISOR_INITIALIZED=1
