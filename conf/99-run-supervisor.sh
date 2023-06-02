b20f0d057961e23fba4aede627990c690dc3ded9
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2023-06-02 09:44:25 +0900

    Fix initialize process

--- conf/99-run-supervisor.sh
+++ conf/99-run-supervisor.sh
@@ -3,19 +3,9 @@
 set -xe
 
 # For Solr
-supervisord -c /home/jovyan/.nbsearch/supervisor.conf
-
-if [[ ! -f /home/$NB_USER/.nbsearch/config_local.py ]] ; then
-    while ! nc -z localhost 8983; do
-      sleep 0.5
-    done
-    while ! nc -z localhost 9000; do
-      sleep 0.5
-    done
-    while ! curl http://localhost:8983/solr/jupyter-cell/admin/ping | grep '"status":"OK"'; do
-      sleep 0.5
-    done
-    jupyter nbsearch update-index --debug /home/$NB_USER/.jupyter/jupyter_notebook_config.py local &
+if [ "$EUID" -eq 0 ]; then
+    cp -f /home/jovyan/.nbsearch/supervisor-root.conf /home/jovyan/.nbsearch/supervisor.conf
 fi
+supervisord -c /home/jovyan/.nbsearch/supervisor.conf
 
 export SUPERVISOR_INITIALIZED=1
